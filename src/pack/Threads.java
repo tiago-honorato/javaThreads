e700876424bb3bb017841f000585b98744ea6865
Tiago Honorato <tiagohirute@gmail.com>
2023-12-20 21:41:45 -0300

    last commit

--- src/pack/Threads.java
+++ src/pack/Threads.java
@@ -4,8 +4,31 @@ public class Threads {
 
 	public static void main(String[] args) {
 		
+		Thread myThead = new Thread(t1);
+		myThead.start();
 		
+		new Thread(t2).start();
 		
 	}
+	
+	public static Runnable t1 = new Runnable() {
+		
+		@Override
+		public void run() {
+			
+		System.out.println("t1");
+			
+		}
+	};
+	
+	public static Runnable t2 = new Runnable() {
+		
+		@Override
+		public void run() {
+			
+			System.out.println("t2");
+			
+		}
+	};
 	
 }
